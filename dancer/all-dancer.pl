77569b50951f4256fb27960f199fb94091a04246
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2013-07-03 14:51:07 +0400

    Removed warnings at app's startup

--- dancer/all-dancer.pl
+++ dancer/all-dancer.pl
@@ -14,7 +14,6 @@ my $api_dir = "$rootdir/API/lib";
 for my $app_path (glob "$api_dir/*.pm") {
     my (undef, $dir, $app) = File::Spec->splitpath($app_path);
     $app =~ s/[.]pm$//;
-    warn "Loading $app from $dir";
     push @INC, $dir;
     load_app($app);
 }
